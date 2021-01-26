350c494e80fc052b36338fd341b78a192f3cd94e
ovkhasch <vkolegkv@gmail.com>
2021-01-27 02:33:38 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "url" {
-  value = "http://${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+  value = "http://${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
 }
